5753dc25e17a0ac7376b0c4b9bdc2546cef3e635
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-02 12:21:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -23,5 +23,7 @@ class Dog
     end
   end
 
-  
+  def save
+    @@all << self
+  end
 end
